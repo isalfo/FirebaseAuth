6f655264a1493476448b77f21adbb2f9dd0bb4e8
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2021-10-01 15:48:37 -0300

    feat Add Facebook login, bug running with simulator, couldn't be fully tested

--- FirebaseTest/AppDelegate.swift
+++ FirebaseTest/AppDelegate.swift
@@ -8,18 +8,29 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import FBSDKCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+  
   var window: UIWindow?
-
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
+    ApplicationDelegate.shared.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
     return true
   }
   
   func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    return GIDSignIn.sharedInstance.handle(url)
+    
+    return GIDSignIn.sharedInstance.handle(url) || ApplicationDelegate.shared.application(
+      app,
+      open: url,
+      sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+      annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+    )
   }
 }
